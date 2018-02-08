24d48670a9bf55700097f362d08393688fb9fb09
zhangke <kaiueo@outlook.com>
2018-02-08 15:38:59 +0800

    Initial Commit

---
+++ ATSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATSS
+//
+//  Created by 张克 on 08/02/2018.
+//  Copyright © 2018 zhangke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
