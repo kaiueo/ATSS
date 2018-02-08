0774352b89cef31acb5c61dcb51805910cdab4b9
zhangke <kaiueo@outlook.com>
2018-02-08 23:19:22 +0800

    UI finished

---
+++ ATSS/ConfigStrings.swift
@@ -0,0 +1,13 @@
+//
+//  ConfigStrings.swift
+//  ATSS
+//
+//  Created by 张克 on 08/02/2018.
+//  Copyright © 2018 zhangke. All rights reserved.
+//
+
+import Foundation
+
+struct StoryBoardConfigs {
+    static let SummaryHistoryTableViewCellIdentifier = "SummaryHistoryTableViewCell"
+}
